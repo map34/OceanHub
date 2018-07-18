f691a3e9e7bb5ee0540904675eaeefc3b8fa2ecf
Adrian Prananda <prananda0203@gmail.com>
2018-07-18 10:31:16 -0700

    Streaming videos work for now, need to implement offset-based pausing mechanism + throttling mechanisms

--- backend/server/oceanhub/scripts/shell.py
+++ backend/server/oceanhub/scripts/shell.py
@@ -1,8 +1,11 @@
+from oceanhub.scripts.producer import test_video
+
 def test_global():
     print('Hi, my name is Si!')
 
 
 def make_shell_context():  # get all the tasks loaded up into the application
     return {
-        'test_global': test_global
+        'test_global': test_global,
+        'test_producer_video': test_video
     }
