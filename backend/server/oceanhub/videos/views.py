99d1e4ccd838e1784b05dbf959336c650352899d
Adrian Prananda <prananda0203@gmail.com>
2018-07-31 23:16:42 -0700

    Fixed docker-compose run not exiting, flake8 compliant code

--- backend/server/oceanhub/videos/views.py
+++ backend/server/oceanhub/videos/views.py
@@ -2,16 +2,18 @@ from flask import Blueprint, Response
 from kafka import KafkaConsumer
 
 videos_blueprint = Blueprint('videos', __name__)
-#Continuously listen to the connection and print messages as recieved
+# Continuously listen to the connection and print messages as recieved
+
 
 @videos_blueprint.route('/')
 def index():
     consumer = KafkaConsumer(bootstrap_servers='kafka:9092',
-                            auto_offset_reset='earliest',
-                            consumer_timeout_ms=1000)
+                             auto_offset_reset='earliest',
+                             consumer_timeout_ms=1000)
     consumer.subscribe(['video-stream'])
     return Response(kafkastream(consumer),
-                        mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 def kafkastream(consumer):
     for msg in consumer:
@@ -19,4 +21,3 @@ def kafkastream(consumer):
         # print(msg.offset)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + msg.value + b'\r\n\r\n')
-
